7374510a01fac15b82d2420cbf74ed0dbd7d4841
Gulnara <gulyasmir@yandex.ru>
2021-01-10 22:58:34 +0300

    make Gravity pull dots

---
+++ script.js
@@ -0,0 +1,117 @@
+(() => {
+    const cnv = document.querySelector(`canvas`);
+    const ctx = cnv.getContext(`2d`);
+    const TWO_PI = 2*Math.PI;
+
+    let w,h, mouse,dots;
+
+    const config = {
+        dotMinRad:6,
+        dotMaxRad:20,
+        massFactor: 0.002,
+        defaultColor:`rgb(240, 122, 38, 0.9)`,
+        smooth: 0.65,
+        sphereRadius:300,
+        bigDotRad :35,
+        mouseSize:120
+    }
+
+
+    class Dot {
+        constructor(radius) {
+            this.pos = {x:mouse.x, y:mouse.y};
+            this.vel = {x:0, y:0};
+            this.rad = radius || random(config.dotMinRad, config.dotMaxRad);
+            this.mass = this.rad * config.massFactor;
+            this.color = config.defaultColor
+        }
+
+        draw(x,y) {
+            this.pos.x  = x || this.pos.x + this.vel.x;
+            this.pos.y  = y || this.pos.y + this.vel.y;
+            createCircle(this.pos.x, this.pos.y, this.rad, true, this.color);
+            createCircle(this.pos.x, this.pos.y, this.rad, false, config.defaultColor);
+        }
+    }
+
+    function  updateDots(){
+        for (let i = 1;i < dots.length; i++){
+            let acc = {x:0, y:0}
+
+            for (let j = 0; j < dots.length; j++){
+                if (i===j) {
+                    continue;
+                }
+                let [a, b] = [dots[i], dots[j]];
+
+                let delta = { x: b.pos.x - a.pos.x, y:b.pos.y - a.pos.y}
+                let dist = Math.sqrt(delta.x * delta.x + delta.y * delta.y) || 1;
+                let force =(dist - config.sphereRadius)/ dist * b.mass;
+
+                if (j == 0) {
+                    let alpha = config.mouseSize / dist;
+                    a.color =`rgb(240, 122, 38, ${alpha})`;
+                    dist < config.mouseSize ? force = (dist - config.mouseSize) * b.mass :  force =  a.mass;
+                }
+
+                acc.x += delta.x * force;
+                acc.y += delta.y * force;
+            }
+
+            dots[i].vel.x = dots[i].vel.x * config.smooth + acc.x * dots[i].mass;
+            dots[i].vel.y = dots[i].vel.y * config.smooth + acc.y * dots[i].mass;
+        }
+
+        dots.map(e => e == dots[0] ? e.draw(mouse.x, mouse.y) : e.draw());
+    }
+
+
+    function  createCircle(x,y,rad,fill,color) {
+        ctx.fillStyle = ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.arc(x,y,rad,0,TWO_PI);
+        ctx.closePath();
+        fill ? ctx.fill() : ctx.stroke();
+    }
+
+    function  random(min, max){
+        return  Math.random() * (max-min) + min;
+    }
+
+    function init(){
+        w = cnv.width = innerWidth;
+        h = cnv.height = innerHeight;
+
+        mouse = {x:w /2, y:h/2, down:false}
+        dots = [];
+
+        dots.push(new Dot(config.bigDotRad));
+    }
+
+    function  loop (){
+        ctx.clearRect(0,0, w, h);
+
+        if (mouse.down)  {
+            dots.push (new Dot());
+        }
+
+        updateDots();
+
+        window.requestAnimationFrame(loop);
+    }
+    init();
+    loop();
+
+    function setPos({layerX, layerY}) {
+        [mouse.x, mouse.y] = [layerX, layerY];
+    }
+
+    function isDown() {
+        mouse.down = !mouse.down;
+    }
+
+    cnv.addEventListener(`mousemove`, setPos);
+    window.addEventListener(`mousedown`, isDown);
+    window.addEventListener(`mouseup`, isDown);
+
+})();
